e0eb5b766f9a65d1e7c1cd6dbe1dbb485a273319
Phil <phl.j.gray@gmail.com>
2025-09-25 22:26:07 -0700

    add server url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://nfc-reader-backend-6iwh.onrender.com";
 
 function App() {
   const [tapHistory, setTapHistory] = useState([]);
